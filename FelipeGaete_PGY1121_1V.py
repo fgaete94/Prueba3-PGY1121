134fe0ec57b26ae4bf59d11fb7777b62b14f547c
fgaete94 <f.gaete@duocuc.cl>
2023-06-23 22:00:52 -0400

    version1.0

--- FelipeGaete_PGY1121_1V.py
+++ FelipeGaete_PGY1121_1V.py
@@ -1,31 +1,70 @@
 import funciones as fn
-print("Bienvenidos al registro de ciudadanos de Andalucia")
-
+print("Bienvenidos al registro de ciudadanos de Andalucía")
 while True:
-    print("seleccione que desea realizar\n")
-    print("1.- Grabar Ciudadano")
-    print("2.- Buscar Ciudadano")
-    print("3.- Imprimir Certificado")
-    print("4.- Salir")
+    try:
+        print("seleccione que desea realizar\n")
+        print("1.- Grabar Ciudadano")
+        print("2.- Buscar Ciudadano")
+        print("3.- Imprimir Certificado")
+        print("4.- Salir")
+        
+        opcion1=int(input())
 
-    opcion1=int(input())
-
-    if opcion1==1:
-        print("ingrese NIF")
-        print("por ejemplo: 99999999-RTX")
-        nif=input().upper()
-        print("ingrese Nombre")
-        nombre=input().upper()
-        print("ingrese edad")
-        edad=int(input())
-        fn.grabar(nif,nombre,edad) 
-    elif opcion1==2:
-        print("ingrese NIF")
-        nif=input().upper()
-        fn.buscar(nif)
-    elif opcion1==3:
-        print()
-    elif opcion1==4:
-        print("Gracias por usar el programa !")
-        print("Felipe Gaete T, Version 1.0")
-        break
+        if opcion1==1:##menu para ingresar ciudadano
+            print("ingrese NIF")
+            print("por ejemplo: 99999999-RTX")
+            nif=input().upper()
+            print("ingrese Nombre: ")
+            nombre=input().upper()
+            print("ingrese edad: ")
+            try:
+                edad=int(input())
+            except:
+                print("Ingresa un Valor Valido \n") 
+            fn.grabar(nif,nombre,edad) 
+        elif opcion1==2:###menu para buscar ciudadanos
+            print("ingrese NIF")
+            nif=input().upper()
+            try:
+                fn.buscar(nif)
+            except:
+                print("El ciudadano no pertenece la Union Europea")
+        elif opcion1==3:##menu para certificados
+            print("ingrese NIF")
+            nif=input().upper()
+            while True:
+                print("1.- Imprimir Certificado de Nacimiento")
+                print("2.- Imprimir Certificado Conyugal")
+                print("3.- Imprimir Certificado de Pertenencia a UE")
+                opcion2=int(input())
+                try:
+                    if opcion2==1:
+                        print("ingrese el nombre de ciudad de nacimiento:")
+                        ciudad=input().upper()
+                        print("ingrece pais de nacimiento: ")
+                        pais=input().upper()
+                        fn.nacimiento(nif,ciudad,pais)
+                    elif opcion2==2:
+                        print("Ingrese Esatado Conyugal del Ciudadano/a: ")
+                        matrimonio=input().upper()
+                        fn.conyugal(nif,matrimonio)
+                    elif opcion2==3:
+                        fn.pertenencia(nif)
+                except:
+                    print("Error el ciudadano no se encuentra en los registros")
+                try:
+                    print("¿Desea Ver Otro Certificado?:   S/N")
+                    opcion3=input().upper()
+                    if opcion3=="N":
+                        break
+                except:
+                    print("Ingresa un Valor Valido \n") 
+        elif opcion1==4:##menu para salir
+            print("Gracias por usar el programa !")
+            print("Felipe Gaete T, Version 1.0")
+            break
+        elif opcion1<1 or opcion1>4:
+            print("Escoge una opcion entre 1 y 4\n")
+    except:
+        print("Ingresa un Valor Valido \n")
+    
--- funciones.py
+++ funciones.py
@@ -14,18 +14,53 @@ def grabar(nif,nombre,edad):
         return
     if edad<0:
         print("la edad ingresada no es valida.")
-    if edad>=0 and longnom>=8 and longnif>0 and longnif==12:
+    if edad>=0 and longnom>=8 and longnif>0 and longnif==12: ##si se cumple esto, se graba al ciudadano/a en el diccionario
         ciudadanos[nif]=nombre,edad
-        print("Registro Exitoso")
+        print("Registro Exitoso\n")
         return
 
 def buscar(nif): ## KeyError
-    if nif== KeyError:
-        print("El ciudadano no pertenece la Union Europea")
+    if nif== KeyError:##si la llave no existe no encuentra al ciudadano/a
+        print("El ciudadano/a no pertenece la Union Europea")
         return
     else:
-        print("El ciudadano de nombre: ",ciudadanos[nif][0])
+        print("El ciudadano/a de nombre: ",ciudadanos[nif][0])
         print("Edad: ", ciudadanos[nif][1])
         print("Si pertenece a la Union Europea")
         return
 
+def nacimiento(nif,ciudad,pais):
+    if nif== KeyError:##si la llave no existe no encuentra al ciudadano
+        print("El ciudadano/a no se encuentra en la base de datos")
+        return
+    else:
+        print("----- Estado de Andalucía -----")
+        print("El gobierno Español certifica que: ")
+        print("Sr/a: ",ciudadanos[nif][0])
+        print("Edad: ",ciudadanos[nif][1])
+        print("Nacido en la ciudad de:",ciudad,"de: ",pais)
+        print("Es Ciudadano Nacido en Union Europea\n")
+        return
+
+def conyugal(nif,matrimonio):
+    if nif== KeyError:##si la llave no existe no encuentra al ciudadano/a
+        print("El ciudadano/a no se encuentra en la base de datos")
+        return
+    else:
+        print("----- Estado de Andalucía -----")
+        print("El gobierno Español certifica que: ")
+        print("Sr/a: ",ciudadanos[nif][0])
+        print("Edad: ",ciudadanos[nif][1])
+        print("Ciudadano de la Union Europea")
+        print("Se encuentra en la condicion de: ",matrimonio)
+        return
+def pertenencia(nif):
+    if nif== KeyError:##si la llave no existe no encuentra al ciudadano/a
+        print("El ciudadano/a no se encuentra en la base de datos")
+        return
+    else:
+        print("----- Estado de Andalucía -----")
+        print("El gobierno Español certifica que: ")
+        print("Sr/a: ",ciudadanos[nif][0])
+        print(" Es Ciudadano/a de la Union Europea") 
+        return
